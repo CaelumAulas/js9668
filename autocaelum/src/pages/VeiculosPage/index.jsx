6363f35e4b68cf1f2ab36b907201e98e275816eb
Jhonatan Jacinto <jhonatanjacinto@gmail.com>
2021-06-22 10:29:37 -0300

    Exemplo AutoCaelum

--- autocaelum/src/pages/VeiculosPage/index.jsx
+++ autocaelum/src/pages/VeiculosPage/index.jsx
@@ -1,7 +1,13 @@
+import MasterLayout from "../../components/MasterLayout";
+import VeiculosList from "../../components/VeiculosList";
+
 export default function VeiculosPage() {
     return (
-        <>
-
-        </>
+        <MasterLayout>
+            <main className="container lista-veiculos">
+                <h1 className="cabecalho-pagina">Veículos</h1>
+                <VeiculosList quantidade={12} mostrarTitulo={false} />
+            </main>
+        </MasterLayout>
     );
 }
